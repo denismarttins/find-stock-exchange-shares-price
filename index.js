22478ac173b02e0f065a4074c2ace5df66773d7a
denismarttins <51491083+denismarttins@users.noreply.github.com>
2020-06-22 10:37:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const readlineSync = require('readline-sync');
-//transition
+
 
 console.log('Robô em funcionamento. Seja bem-vindo!!');
 
@@ -28,17 +28,4 @@ async function robo(){
 
 
 
-/*const axios = require('axios');
-const cheerio = require('cheerio');
-const url = 'https://www.covabra.com.br/ofertas-bem-estar';
-
-axios(url)
-    .then(response =>{
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const tableStriped = $('card-oferta bem-estar');
-        console.log(tableStriped.length);
-        console.log(html)
-})
-.catch(console.error);*/
 
