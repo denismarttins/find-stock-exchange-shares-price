b43080f426ee2f3f3c127015f99ba7b077cea66e
denismarttins <51491083+denismarttins@users.noreply.github.com>
2020-06-22 10:01:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
+//transition
+
+console.log('Robô em funcionamento. Seja bem-vindo!!');
+
+async function robo(){
+    const browser = await puppeteer.launch({headless:true}); //if headless is false, puppeteer will open a new window in chromium
+    const page = await browser.newPage(); //in order to open a new tabe on the page
+    const acao = readlineSync.question("Informe o codigo da acao: ");
+    
+    const url = `https://www.google.com/search?ei=QiXtXsukGsPC5OUPteeK0A4&q=${acao}&oq=${acao}&gs_lcp=CgZwc3ktYWIQAzIMCAAQgwEQQxBGEPoBMgUIABCDATIFCAAQgwEyAggAMgUIABCDATIFCAAQgwEyAggAMgIIADIFCAAQgwEyBQgAELEDOgQIABBHOgcIABCxAxBDOgQIABBDOgkIABBDEEYQ-gE6BwgAEIMBEENQ6IEHWN-1B2CguwdoAXABeACAAbcDiAHVD5IBCTAuNS4zLjAuMZgBAKABAaoBB2d3cy13aXqwAQA&sclient=psy-ab&ved=0ahUKEwjLzaPu4I7qAhVDIbkGHbWzAuoQ4dUDCAw&uact=5`;
+    await page.goto(url);
+    await page.screenshot({path: 'resultadodabusca.png'}); //code to print result - in this case, return a "png" file.
+    
+    const resultado = await page.evaluate(() => {
+        return document.querySelector('.IsqQVc.NprOob.XcVN5d').innerText;
+    });
+       
+    console.log(`O valor de ${acao} é ${resultado}`);
+    await browser.close(true);
+   }
+
+   robo();
+
+
+
+
+
+
+/*const axios = require('axios');
+const cheerio = require('cheerio');
+const url = 'https://www.covabra.com.br/ofertas-bem-estar';
+
+axios(url)
+    .then(response =>{
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const tableStriped = $('card-oferta bem-estar');
+        console.log(tableStriped.length);
+        console.log(html)
+})
+.catch(console.error);*/
+
